16b4cc9237fb4f1330b57c27571bdb4ee057d4cc
GulicLuka <luka.gulic2@gmail.com>
2023-02-09 17:03:11 +0100

    Create UsersConsole.py
    Created console for users functions

---
+++ UsersConsole.py
@@ -0,0 +1,70 @@
+import os
+
+class UserConsole:
+
+    def userFunctionsMenu():
+        if os.name == "posix":
+            os.system("clear")
+        else:
+            os.system("cls")
+
+        while True:
+            print("*******************************************")
+            print()
+            print("USERS")
+            print("Select function:")
+            print("1:   Get all Users")
+            print("2:   Get a Single User")
+            print("3:   Search Users")
+            print("4:   Filter Users")
+            print("5:   Limit & Skip Users")
+            print("6:   Get User's carts")
+            print("7:   Get User's posts")
+            print("8:   Get User's todos")
+            print("9:   Add a new User")
+            print("10:  Update an existing User")
+            print("11:  Delete User")
+            print("0:   Back")
+            print()
+            print("*******************************************")
+
+            option = input("Input your selection: ")
+
+            if option == "1":
+                # get all users
+                pass
+            elif option == "2":
+                # get a single user
+                pass
+            elif option == "3":
+                # search user
+                pass           
+            elif option == "4":
+                # filter users
+                pass
+            elif option == "5":
+                # limit & skip user
+                pass
+            elif option == "6":
+                # get user's carts
+                pass
+            elif option == "7":
+                # get user's posts
+                pass
+            elif option == "8":
+                # get user's todos
+                pass
+            elif option == "9":
+                # add user
+                pass
+            elif option == "10":
+                # update user
+                pass
+            elif option == "11":
+                # delete user
+                pass
+            elif option == "0":
+                # go back
+                return
+            else:
+                print("Invalid option, try selecting number from 0 to 11")
